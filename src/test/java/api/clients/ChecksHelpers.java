629a91e5f1f1a4498212ba88dcffb4041d74d2a8
hukumkaserkov <nikitadel@yandex.ru>
2026-09-04 10:18:33 +0300

    add revisions to the check helpers class

--- src/test/java/api/clients/ChecksHelpers.java
+++ src/test/java/api/clients/ChecksHelpers.java
@@ -21,12 +21,12 @@ public class ChecksHelpers {
         // Добавляем новый файл требований
         String idRequirementsFile = requirementsHelpers.getRequirementId();
         // Создаем на основе новых требований файл ids
-        String endpoint = "/ws-requirements/v1/ids/create";
+        String endpoint = "/requirement/v1/id/create";
         IdsRequirementsAttributes idsRequirementsAttributes =
-                new IdsRequirementsAttributes(List.of("9da38c83-7a89-46c7-b8c0-1dca3027549e"),
-                        "499db5c7-702f-4809-aab3-a0a697731745",
-                        "915f8afc-9d13-41da-a642-4a703f8a5e15",
-                        List.of("b38013d7-590a-4780-bc08-849e7e263d32", "1dc87342-f089-48e3-8d17-cadd01bf7e1a"));
+                new IdsRequirementsAttributes(List.of("9da78c83-7a89-46c7-b8c0-1dca3027549e"),
+                        "499db5c7-702f-4809-aab3-a0a447731745",
+                        "915f8afc-9d13-41da-a642-4a722f8a5e15",
+                        List.of("b38013d7-590a-4780-bc08-849e7e856d32", "1dc87342-f119-48e3-8d17-cadd01bf7e1a"));
         CreateNewIds body = new CreateNewIds(idRequirementsFile, idsRequirementsAttributes, true);
         File schema = new File("src/test/resources/schemas/create-ids-schema.json");
         return transport.postRequestWithToken(endpoint, body, schema, 200);
@@ -34,13 +34,13 @@ public class ChecksHelpers {
 
     public Response startIdsCheck(String appealId, String idsFileId, AdvsrchFilesDto firstFile){
 
-        String endpoint = "/ws-requirements/v1/ids/start-check";
+        String endpoint = "/requirement/v1/id/start-check";
         IdsCheckStartDto body = new IdsCheckStartDto(appealId, idsFileId , List.of(firstFile.getFileId()));
         return transport.postRequestNoSchema(endpoint, body, 200);
     }
 
     public String getConclusionId(String appealId){
-        String endpoint = "/ws-appeals/v1/appeals/{appealId}/conclusion/reports";
+        String endpoint = "/appeal/v1/appeals/{appealId}/conclusion/report";
         Response response = transport.getRequestWithParamsNoChecks(endpoint,
                 "appealId", appealId);
         return response.jsonPath().getString("conclusionId");
